1ec6296d9864193296678520554d490e324d3048
KSClopton <kyle.clopton@outlook.com>
2020-08-14 13:33:54 -0400

    Added somemore structure including the services for todo list

--- todos/src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
+++ todos/src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.lambdaschool.todos.repository;
 
-public interface TodoRepository {
+import com.lambdaschool.todos.models.Todos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository<Todos,Long> {
+
+
 }
--- todos/src/main/java/com/lambdaschool/todos/services/TodosServiceImp.java
+++ todos/src/main/java/com/lambdaschool/todos/services/TodosServiceImp.java
@@ -1,12 +1,13 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service(value = "todosServices")
-public interface TodosServiceImp implements TodosService{
+public class TodosServiceImp implements TodosService{
     @Autowired
     private TodoRepository todorepos;
 
